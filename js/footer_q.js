c5abf03d7aadb977f86fa714ab3cc74fd9d62d42
sidatube <70898455+sidatube@users.noreply.github.com>
2020-12-29 13:15:57 +0700

    thanh

---
+++ js/footer_q.js
@@ -0,0 +1,168 @@
+function footer() {
+    return"<div class=\"full1\">\n" +
+        "        <div class=\"container\">\n" +
+        "            <div class=\"doan1\"></div>\n" +
+        "        </div>\n" +
+        "        <div class=\"container\">\n" +
+        "            <div class=\"doan2\">\n" +
+        "                <div class=\"container\">\n" +
+        "                    <div class=\"kho1\">\n" +
+        "                        <div class=\"col-md-2\">\n" +
+        "                            <img src=\"imgs/SEOULISTA-BLACK.png\" height=\"60\" width=\"150\"/>\n" +
+        "                        </div>\n" +
+        "                    </div>\n" +
+        "                    <div class=\"kho2\">\n" +
+        "                        <div class=\"col-md-6\">\n" +
+        "                            <div class=\"thanhdai\">\n" +
+        "                                <div class=\"col-md-9\">\n" +
+        "                                    <div class=\"dangki1\">\n" +
+        "                                        Đăng ký nhận bản tin email từ Seoulista\n" +
+        "                                    </div>\n" +
+        "                                </div>\n" +
+        "                                <div class=\"col-md-3\">\n" +
+        "                                    <div class=\"dangki2\">\n" +
+        "                                        ĐĂNG KÍ NGAY\n" +
+        "                                    </div>\n" +
+        "                                </div>\n" +
+        "                            </div>\n" +
+        "                        </div>\n" +
+        "                    </div>\n" +
+        "                    <div class=\"kho3\">\n" +
+        "                        <div class=\"col-md-3\">\n" +
+        "                            <div class=\"trongkho3\">\n" +
+        "                                <ul class=\"icon\">\n" +
+        "                                    <li>\n" +
+        "                                        <a class=\"iconx1\" href=\"#\"><i class=\"fab fa-facebook-f top-right-1-1\"></i></a>\n" +
+        "                                    </li>\n" +
+        "                                    <li>\n" +
+        "                                        <a class=\"iconx2\" href=\"#\"><i class=\"fab fa-instagram top-right-1-2\"></i></a>\n" +
+        "                                    </li>\n" +
+        "                                    <li>\n" +
+        "                                        <a class=\"iconx3\" href=\"#\"><i class=\"fab fa-youtube\"></i></a>\n" +
+        "                                    </li>\n" +
+        "                                </ul>\n" +
+        "                            </div>\n" +
+        "                        </div>\n" +
+        "                    </div>\n" +
+        "                </div>\n" +
+        "            </div>\n" +
+        "        </div>\n" +
+        "        <div class=\"container\">\n" +
+        "            <div class=\"doan3\"></div>\n" +
+        "        </div>\n" +
+        "        <div class=\"col-md-12\">\n" +
+        "            <div class=\"container\">\n" +
+        "                <div class=\"doan4\" >\n" +
+        "                    <div class=\"gach\">\n" +
+        "                    </div>\n" +
+        "                </div>\n" +
+        "            </div>\n" +
+        "        </div>\n" +
+        "    </div>\n" +
+        "    <div class=\"full2\">\n" +
+        "        <div class=\"container\">\n" +
+        "            <div class=\"doan3\"></div>\n" +
+        "        </div>\n" +
+        "        <div class=\"container\">\n" +
+        "            <div class=\"col-md-4\">\n" +
+        "                <div class=\"gom1\">\n" +
+        "                    <p class=\"songsong\">\n" +
+        "                        Seoulista Vietnam\n" +
+        "                    </p>\n" +
+        "                    <p class=\"Seoulista2\">\n" +
+        "                        Seoulista – là một công ty đi theo mô hình\n" +
+        "                        “Best Selected”, tức là lựa chọn những sản\n" +
+        "                        phẩm làm đẹp tốt nhất từ các thương hiệu được\n" +
+        "                        đánh giá cao tại Hàn Quốc để đem đến cho\n" +
+        "                        khách hàng Việt Nam.\n" +
+        "                    </p>\n" +
+        "                </div>\n" +
+        "            </div>\n" +
+        "            <div class=\"col-md-2\">\n" +
+        "                <div class=\"gom2\">\n" +
+        "                    <p class=\"songsong\">\n" +
+        "                        Giới thiệu\n" +
+        "                    </p>\n" +
+        "                    <p><a class=\"menugioithieu\" href=\"#\">Về Seoulista.vn</a></p>\n" +
+        "                    <p><a class=\"menugioithieu\" href=\"#\">Về Blog Soulmate.vn</a></p>\n" +
+        "                    <p><a class=\"menugioithieu\" href=\"#\">Sitemap</a></p>\n" +
+        "                    <p><a class=\"menugioithieu\" href=\"#\">Liên hệ</a></p>\n" +
+        "                </div>\n" +
+        "            </div>\n" +
+        "            <div class=\"col-md-2\">\n" +
+        "                <div class=\"gom2\">\n" +
+        "                    <p class=\"songsong\">\n" +
+        "                        Các Quy định\n" +
+        "                    </p>\n" +
+        "                    <p><a class=\"menugioithieu\" href=\"#\">Điều khoản sử dụng</a></p>\n" +
+        "                    <p><a class=\"menugioithieu\" href=\"#\">Chính sách bảo mật</a></p>\n" +
+        "                    <p><a class=\"menugioithieu\" href=\"#\">Chính sách mua hàng</a></p>\n" +
+        "                    <p><a class=\"menugioithieu\" href=\"#\">Chính sách giao hàng</a></p>\n" +
+        "                </div>\n" +
+        "            </div>\n" +
+        "            <div class=\"col-md-2\">\n" +
+        "                <div class=\"gom2\">\n" +
+        "                    <p class=\"songsong\">\n" +
+        "                        Hướng dẫn & FAQ\n" +
+        "                    </p>\n" +
+        "                    <p><a class=\"menugioithieu\" href=\"#\">Cách mua hàng Online</a></p>\n" +
+        "                    <p><a class=\"menugioithieu\" href=\"#\">Các hình thức thanh toán</a></p>\n" +
+        "                    <p><a class=\"menugioithieu\" href=\"#\">Đổi trả hàng hóa</a></p>\n" +
+        "                    <p><a class=\"menugioithieu\" href=\"#\">Khuyến mại và tích điểm</a></p>\n" +
+        "                </div>\n" +
+        "            </div>\n" +
+        "            <div class=\"col-md-2\">\n" +
+        "                <div class=\"gom2\">\n" +
+        "                    <p class=\"songsong\">\n" +
+        "                        Cách mua hàng\n" +
+        "                    </p>\n" +
+        "                    <p><a class=\"menugioithieu\" href=\"#\">Sản phẩm mới về</a></p>\n" +
+        "                    <p><a class=\"menugioithieu\" href=\"#\">Sản phẩm bán chạy</a></p>\n" +
+        "                    <p><a class=\"menugioithieu\" href=\"#\">Sản phẩm giảm giá</a></p>\n" +
+        "                    <p><a class=\"menugioithieu\" href=\"#\">Khuyến mãi và Quà tặng</a></p>\n" +
+        "                </div>\n" +
+        "            </div>\n" +
+        "        </div>\n" +
+        "        <div class=\"container\">\n" +
+        "            <div class=\"doan3\"></div>\n" +
+        "        </div>\n" +
+        "        <div class=\"col-md-12\">\n" +
+        "            <div class=\"container\">\n" +
+        "                <div class=\"doan4\" >\n" +
+        "                    <div class=\"gach\">\n" +
+        "                    </div>\n" +
+        "                </div>\n" +
+        "            </div>\n" +
+        "        </div>\n" +
+        "    </div>\n" +
+        "    <div class=\"full3\">\n" +
+        "        <div class=\"col-md-12\">\n" +
+        "            <div class=\"container\">\n" +
+        "                <div class=\"doan4\" >\n" +
+        "                    <div class=\"gach\">\n" +
+        "                    </div>\n" +
+        "                </div>\n" +
+        "            </div>\n" +
+        "        </div>\n" +
+        "        <div class=\"container\">\n" +
+        "            <div class=\"doan3\"></div>\n" +
+        "        </div>\n" +
+        "        <div class=\"nhahieu\">\n" +
+        "            <div class=\"container\">\n" +
+        "                <div class=\"col-md-2\">\n" +
+        "                    <img src=\"imgs/dathongbao.png\" height=\"60\" width=\"150\"/>\n" +
+        "                </div>\n" +
+        "                <div class=\"col-md-10\">\n" +
+        "                    <div class=\"ttnhanhieu\">\n" +
+        "                        Công ty TNHH Mỹ phẩm Seoulista Việt Nam. Địa chỉ: 380 phố Đội Cấn, P. Cống Vị, Q.Ba Đình, Hà Nội, Việt Nam. Email: sales@seoulista.co. Số ĐKKD: 0107949129 – Ngày cấp: 07/08/2017, được sửa đổi lần thứ nhất, ngày 13/04/2018. Cơ quan cấp: Sở Kế hoạch và Đầu tư Thành phố Hà Nội.\n" +
+        "                    </div>\n" +
+        "                </div>\n" +
+        "            </div>\n" +
+        "        </div>\n" +
+        "        <div class=\"container\">\n" +
+        "            <div class=\"doan1\">\n" +
+        "            </div>\n" +
+        "        </div>\n" +
+        "    </div>"
+}
+document.getElementById("footer").innerHTML=footer();
