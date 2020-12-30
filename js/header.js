a1d0ae643ca0e32d90135bfaf6fa87434454049b
hoang <hoangtu2378@gmail.com>
2020-12-30 13:39:02 +0700

    tu

---
+++ js/header.js
@@ -0,0 +1,25 @@
+function header() {
+    return"<div class=\"section header\">\n" +
+        "        <div id=\"top\" class=\"header-1\">\n" +
+        "            <div class=\"container\">\n" +
+        "                <div class=\"top-left-1\">\n" +
+        "                    <div class=\"text-top\">Free Shipping nội thành từ 500k, tỉnh thành khác từ 1.000K</div>\n" +
+        "                </div>\n" +
+        "                <div class=\"top-right-1\">\n" +
+        "                    <ul>\n" +
+        "                        <li>\n" +
+        "                            <a class=\"hotline\" href=\"\">HOTLINE 0827 555 916</a>\n" +
+        "                        </li>\n" +
+        "                        <li>\n" +
+        "                            <a class=\"icon-top-2\" href=\"\"><i class=\"fab fa-facebook-messenger\"></i></a>\n" +
+        "                        </li>\n" +
+        "                        <li>\n" +
+        "                            <a class=\"zalo\" href=\"\">ZALO</a>\n" +
+        "                        </li>\n" +
+        "                    </ul>\n" +
+        "                </div>\n" +
+        "            </div>\n" +
+        "        </div>\n" +
+        "    </div>"
+}
+document.getElementById("header").innerHTML=header();
---
+++ js/nav.js
@@ -0,0 +1,231 @@
+function nav(){
+    return"<div class=\"nav-1\">\n" +
+        "            <div class=\"container\">\n" +
+        "                <div class=\"top-left-2\">\n" +
+        "                    <a href=\"\"><img src=\"imgs/seoulista-logo.png\" width=\"116\" height=\"44\" /></a>\n" +
+        "                </div>\n" +
+        "                <div class=\"top-left-3\">\n" +
+        "                    <ul>\n" +
+        "                        <li>\n" +
+        "                            <div class=\"top-left-3-0\">\n" +
+        "                                <a class=\"top-left-3-1 text-ct\" href=\"\">MUA HÀNG</a> <i class=\"fas fa-angle-down\"></i>\n" +
+        "                            </div>\n" +
+        "                            <ul class=\"menu\">\n" +
+        "                                <li class=\"menu-1\">\n" +
+        "                                    <a class=\"menu-1-1\" href=\"\">CHĂM SÓC DA</a>\n" +
+        "                                    <ul class=\"menu-1-2\">\n" +
+        "                                        <li>\n" +
+        "                                            <div class=\"menu-1-3\">\n" +
+        "                                                <a class=\"menu-1-4\" href=\"\">Quy trình dưỡng da</a> <i class=\"fas fa-angle-right \"></i>\n" +
+        "                                            </div>\n" +
+        "                                            <ul class=\"menu-1-5\">\n" +
+        "                                                <li>\n" +
+        "                                                    <div class=\"menu-1-6\"><a class=\"menu-1-7\" href=\"\">Làm sạch</a> <i class=\"fas fa-angle-right \"></i></div>\n" +
+        "                                                    <ul class=\"menu-1-8\">\n" +
+        "                                                        <li><a href=\"\">Tẩy trang</a></li>\n" +
+        "                                                        <li><a href=\"\">Sữa rửa mặt</a></li>\n" +
+        "                                                    </ul>\n" +
+        "                                                </li>\n" +
+        "                                                <li>\n" +
+        "                                                    <div class=\"menu-1-6\"><a class=\"menu-1-7\" href=\"\">Tẩy da chết</a> <i class=\"fas fa-angle-right \"></i></div>\n" +
+        "                                                    <ul class=\"menu-1-11\">\n" +
+        "                                                        <li><a href=\"\">Tẩy da chết hóa học</a></li>\n" +
+        "                                                        <li><a href=\"\">Tẩy da chết cơ học</a></li>\n" +
+        "                                                    </ul>\n" +
+        "                                                </li>\n" +
+        "                                                <li><a class=\"\" href=\"\">Nước cân bằng</a></li>\n" +
+        "                                                <li><a class=\"\" href=\"\">Tinh chất</a></li>\n" +
+        "                                                <li>\n" +
+        "                                                    <div class=\"menu-1-6\"><a class=\"menu-1-7\" href=\"\">Dưỡng ẩm</a> <i class=\"fas fa-angle-right \"></i></div>\n" +
+        "                                                    <ul class=\"menu-1-12\">\n" +
+        "                                                        <li><a href=\"\">Kem dưỡng</a></li>\n" +
+        "                                                        <li><a href=\"\">Xịt khoáng</a></li>\n" +
+        "                                                        <li><a href=\"\">Mặt nạ ngủ</a></li>\n" +
+        "                                                    </ul>\n" +
+        "                                                </li>\n" +
+        "                                                <li><a class=\"\" href=\"\">Đặc trị</a></li>\n" +
+        "                                                <li><a class=\"\" href=\"\">Dưỡng mắt</a></li>\n" +
+        "                                                <li>\n" +
+        "                                                    <div class=\"menu-1-6\"><a class=\"menu-1-7\" href=\"\">Mặt nạ</a> <i class=\"fas fa-angle-right \"></i></div>\n" +
+        "                                                    <ul class=\"menu-1-13\">\n" +
+        "                                                        <li><a href=\"\">Mặt nạ giấy</a></li>\n" +
+        "                                                        <li><a href=\"\">Mặt nạ rửa</a></li>\n" +
+        "                                                    </ul>\n" +
+        "                                                </li>\n" +
+        "                                                <li><a class=\"\" href=\"\">Kem chống nắng</a></li>\n" +
+        "                                                <li><a class=\"\" href=\"\">SET dưỡng da</a></li>\n" +
+        "                                            </ul>\n" +
+        "                                        </li>\n" +
+        "                                        <li>\n" +
+        "                                            <div class=\"menu-1-3\">\n" +
+        "                                                <a class=\"menu-1-4\" href=\"\">Các vấn đề về da</a> <i class=\"fas fa-angle-right \"></i>\n" +
+        "                                            </div>\n" +
+        "                                            <ul class=\"menu-1-9\">\n" +
+        "                                                <li>\n" +
+        "                                                    <div class=\"\"><a class=\"\" href=\"\">Điều trị mụn</a></div>\n" +
+        "                                                </li>\n" +
+        "                                                <li>\n" +
+        "                                                    <div class=\"\"><a class=\"\" href=\"\">Thu nhỏ lỗ chân lông</a></div>\n" +
+        "                                                </li>\n" +
+        "                                                <li>\n" +
+        "                                                    <div class=\"\"><a class=\"\" href=\"\">Trị vết thâm, nám và tàn nhang</a></div>\n" +
+        "                                                </li>\n" +
+        "                                                <li>\n" +
+        "                                                    <div class=\"\"><a class=\"\" href=\"\">Dưỡng trắng và làm sáng da</a></div>\n" +
+        "                                                </li>\n" +
+        "                                                <li>\n" +
+        "                                                    <div class=\"\"><a class=\"\" href=\"\">Cấp nước dưỡng ẩm</a></div>\n" +
+        "                                                </li>\n" +
+        "                                                <li>\n" +
+        "                                                    <div class=\"\"><a class=\"\" href=\"\">Chống lão hóa và nếp nhăn</a></div>\n" +
+        "                                                </li>\n" +
+        "                                            </ul>\n" +
+        "                                        </li>\n" +
+        "                                        <li>\n" +
+        "                                            <div class=\"menu-1-3\">\n" +
+        "                                                <a class=\"menu-1-4\" href=\"\">Các loại da</a> <i class=\"fas fa-angle-right \"></i>\n" +
+        "                                            </div>\n" +
+        "                                            <ul class=\"menu-1-10\">\n" +
+        "                                                <li>\n" +
+        "                                                    <div class=\"\"><a class=\"\" href=\"\">Da thường</a></div>\n" +
+        "                                                </li>\n" +
+        "                                                <li>\n" +
+        "                                                    <div class=\"\"><a class=\"\" href=\"\">Da khô</a></div>\n" +
+        "                                                </li>\n" +
+        "                                                <li>\n" +
+        "                                                    <div class=\"\"><a class=\"\" href=\"\">Da dầu, mụn</a></div>\n" +
+        "                                                </li>\n" +
+        "                                                <li>\n" +
+        "                                                    <div class=\"\"><a class=\"\" href=\"\">Da hỗn hợp</a></div>\n" +
+        "                                                <li>\n" +
+        "                                                    <div class=\"\"><a class=\"\" href=\"\">Da nhạy cảm</a></div>\n" +
+        "                                                </li>\n" +
+        "                                                <li>\n" +
+        "                                                    <div class=\"\"><a class=\"\" href=\"\">Da lão hóa</a></div>\n" +
+        "                                                </li>\n" +
+        "                                            </ul>\n" +
+        "                                        </li>\n" +
+        "                                    </ul>\n" +
+        "                                </li>\n" +
+        "                                <li class=\"menu-2\">\n" +
+        "                                    <a class=\"menu-1-1\" href=\"\">TRANG ĐIỂM</a>\n" +
+        "                                    <ul class=\"menu-1-2\">\n" +
+        "                                        <li>\n" +
+        "                                            <div class=\"menu-1-3\">\n" +
+        "                                                <a class=\"menu-1-4\" href=\"\">Trang điểm nền</a> <i class=\"fas fa-angle-right \"></i>\n" +
+        "                                            </div>\n" +
+        "                                            <ul class=\"menu-2-1\">\n" +
+        "                                                <li><a class=\"\" href=\"\">Kem lót</a></li>\n" +
+        "                                                <li><a class=\"\" href=\"\">Kem nền</a></li>\n" +
+        "                                                <li><a class=\"\" href=\"\">Che khuyết điểm</a></li>\n" +
+        "                                                <li><a class=\"\" href=\"\">Phấn phủ</a></li>\n" +
+        "                                                <li><a class=\"\" href=\"\">Phấn má</a></li>\n" +
+        "                                                <li><a class=\"\" href=\"\">Tạo khối & Bắt sáng</a></li>\n" +
+        "                                            </ul>\n" +
+        "                                        </li>\n" +
+        "                                    </ul>\n" +
+        "                                    <ul class=\"menu-1-2\">\n" +
+        "                                        <li>\n" +
+        "                                            <div class=\"menu-1-3\">\n" +
+        "                                                <a class=\"menu-1-4\" href=\"\">Trang điểm mắt</a> <i class=\"fas fa-angle-right \"></i>\n" +
+        "                                            </div>\n" +
+        "                                            <ul class=\"menu-2-2\">\n" +
+        "                                                <li><a class=\"\" href=\"\">Phấn mắt</a></li>\n" +
+        "                                                <li><a class=\"\" href=\"\">Kẻ chân mày</a></li>\n" +
+        "                                            </ul>\n" +
+        "                                        </li>\n" +
+        "                                    </ul>\n" +
+        "                                    <ul class=\"menu-1-2\">\n" +
+        "                                        <li>\n" +
+        "                                            <div class=\"menu-1-3\">\n" +
+        "                                                <a class=\"menu-1-4\" href=\"\">Son môi</a> <i class=\"fas fa-angle-right \"></i>\n" +
+        "                                            </div>\n" +
+        "                                            <ul class=\"menu-2-3\">\n" +
+        "                                                <li><a class=\"\" href=\"\">Son thỏi</a></li>\n" +
+        "                                                <li><a class=\"\" href=\"\">Son bóng</a></li>\n" +
+        "                                                <li><a class=\"\" href=\"\">Kẻ môi</a></li>\n" +
+        "                                                <li><a class=\"\" href=\"\">Son kem</a></li>\n" +
+        "                                                <li><a class=\"\" href=\"\">Son tint</a></li>\n" +
+        "                                                <li><a class=\"\" href=\"\">Son dưỡng môi</a></li>\n" +
+        "                                            </ul>\n" +
+        "                                        </li>\n" +
+        "                                    </ul>\n" +
+        "                                </li>\n" +
+        "                                <li class=\"menu-3\">\n" +
+        "                                    <a class=\"menu-1-1\" href=\"\">CỌ & PHỤ KIỆN</a>\n" +
+        "                                    <ul class=\"menu-3-1\">\n" +
+        "                                        <li>\n" +
+        "                                            <div class=\"\"><a class=\"\" href=\"\">Trang điểm nền</a></div>\n" +
+        "                                        </li>\n" +
+        "                                        <li>\n" +
+        "                                            <div class=\"\"><a class=\"\" href=\"\">Phụ kiện khác</a></div>\n" +
+        "                                        </li>\n" +
+        "                                    </ul>\n" +
+        "                                </li>\n" +
+        "                            </ul>\n" +
+        "                        </li>\n" +
+        "                        <li>\n" +
+        "                            <div class=\"top-left-3-0\">\n" +
+        "                                <a class=\"top-left-3-1 text-ct\" href=\"\">THƯƠNG HIỆU</a> <i class=\"fas fa-angle-down\"></i>\n" +
+        "                            </div>\n" +
+        "                            <ul class=\"menu\">\n" +
+        "                                <li class=\"menu-1\">\n" +
+        "                                    <a class=\"menu-1-1\" href=\"\">THƯƠNG HIỆU</a>\n" +
+        "                                    <ul class=\"menu-4-1\">\n" +
+        "                                        <li><a href=\"\">ACWELL</a></li>\n" +
+        "                                        <li><a href=\"\">COSRX</a></li>\n" +
+        "                                        <li><a href=\"\">BE PLAIN</a></li>\n" +
+        "                                        <li><a href=\"\">DR.CEURACLE</a></li>\n" +
+        "                                        <li><a href=\"\">MAKE P:REM</a></li>\n" +
+        "                                    </ul>\n" +
+        "                                </li>\n" +
+        "                                <li class=\"menu-1\">\n" +
+        "                                    <a class=\"menu-1-1\" href=\"\">THƯƠNG HIỆU</a>\n" +
+        "                                    <ul class=\"menu-4-1\">\n" +
+        "                                        <li><a href=\"\">ISNTREE</a></li>\n" +
+        "                                        <li><a href=\"\">REAL BARRIER</a></li>\n" +
+        "                                        <li><a href=\"\">PURITO</a></li>\n" +
+        "                                        <li><a href=\"\">ROUND LAB</a></li>\n" +
+        "                                        <li><a href=\"\">ROVECTIN</a></li>\n" +
+        "                                    </ul>\n" +
+        "                                </li>\n" +
+        "                            </ul>\n" +
+        "                        </li>\n" +
+        "                        <li>\n" +
+        "                            <div class=\"top-left-3-0\">\n" +
+        "                                <a class=\"top-left-3-1 text-ct\" href=\"\">GIẢM GIÁ</a></i>\n" +
+        "                            </div>\n" +
+        "                        </li>\n" +
+        "                        <li>\n" +
+        "                            <div class=\"top-left-3-0\">\n" +
+        "                                <a class=\"top-left-3-1 text-ct\" href=\"\">K-BEAUTY BLOG</a> <i class=\"fas fa-angle-down\"></i>\n" +
+        "                            </div>\n" +
+        "                            <ul class=\"menu\">\n" +
+        "                                <li class=\"menu-1\">\n" +
+        "                                    <ul class=\"menu-4-1\">\n" +
+        "                                        <li><a href=\"\">Skin Care</a></li>\n" +
+        "                                        <li><a href=\"\">Make up</a></li>\n" +
+        "                                        <li><a href=\"\">Lifestyle</a></li>\n" +
+        "                                        <li><a href=\"\">Reviews</a></li>\n" +
+        "                                        <li><a href=\"\">Seoulista Tips</a></li>\n" +
+        "                                    </ul>\n" +
+        "                                </li>\n" +
+        "                            </ul>\n" +
+        "                        </li>\n" +
+        "                    </ul>\n" +
+        "                </div>\n" +
+        "                <div class=\"top-right-2\">\n" +
+        "                    <li class=\"fr\">\n" +
+        "                        <i class=\"fa-15z fas fa-search\"></i>\n" +
+        "                    </li>\n" +
+        "                    <li class=\"fr\">\n" +
+        "                        <i class=\"fa-15z far fa-user-circle\"></i>\n" +
+        "                    </li>\n" +
+        "                    <li class=\"fr\">\n" +
+        "                        <i class=\"fa-15z fas fa-shopping-cart\"></i>\n" +
+        "                    </li>\n" +
+        "                </div>\n" +
+        "            </div>\n" +
+        "        </div>"
+}
+document.getElementById("nav").innerHTML=nav();
